d3321dc9f5cf818988f80725fad7e7d0fbb5aad3
Ricardo Rivera <r.rivera@live.de>
2018-02-22 18:21:24 +0100

    only LC_ALL in setlocale
    set LC_ALL in humanReadable

--- src/RRule.php
+++ src/RRule.php
@@ -2337,8 +2337,7 @@ class RRule implements RRuleInterface
 		if ( $opt['use_intl'] ) {
 			$default_opt['locale'] = \Locale::getDefault();
 		} else {
-			$category = defined('LC_MESSAGES') ? LC_MESSAGES : LC_ALL;
-			$default_opt['locale'] = setlocale($category, 0);
+			$default_opt['locale'] = setlocale(LC_ALL, 0);
 			if ( $default_opt['locale'] == 'C' ) {
 				$default_opt['locale'] = 'en';
 			}
