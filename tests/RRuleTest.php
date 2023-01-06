fa13bf3f6b411536705dc4ceed8fe2464f4b6e25
Joel Stein <joel@mediatrix.digital>
2023-01-06 04:01:00 -0600

    Fix #120 Reset time to midnight to fix timezone offsets.

--- tests/RRuleTest.php
+++ tests/RRuleTest.php
@@ -1815,6 +1815,19 @@ class RRuleTest extends TestCase
 		$this->assertEquals($count, $rrule->count());
 	}
 
+	/**
+	 * Tests timezone transition in Daylight Savings Time switch.
+	 */
+	public function testDST()
+	{
+		$rrule = new RRule([
+			'FREQ' => 'WEEKLY',
+			'DTSTART' => new \DateTime('2022-10-30T01:00', new \DateTimeZone('America/Chicago')),
+			'COUNT' => 2,
+		]);
+		$this->assertSame('2022-11-06T01:00:00-05:00 CDT 1667714400', $rrule[1]->format('c T U'));
+	}
+
 ///////////////////////////////////////////////////////////////////////////////
 // GetOccurrences
 
